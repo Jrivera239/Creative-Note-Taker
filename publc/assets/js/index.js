ce6c941b5c0920535b51435b7c8beeb49c3030de
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-08-22 12:38:47 -0400

    indexjs

--- publc/assets/js/index.js
+++ publc/assets/js/index.js
@@ -1,8 +1,8 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
@@ -25,30 +25,28 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () =>
-  fetch('/api/notes', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+const getNotes = () => {
+    return $.ajax({
+      url: "/api/notes",
+      method: "GET",
+    });
+  };
 
-const saveNote = (note) =>
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(note),
-  });
+const saveNote = (note) => {
+    return $.ajax({
+      url: "/api/notes",
+      data: note,
+      method: "POST",
+    });
+  };
+  
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+const deleteNote = (id) => {
+    return $.ajax({
+    url: "api/notes/" + id,
+    method: "DELETE",
   });
+};
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
