b550ac3b16a6bfd3968d1050e52d4cbc5b96132d
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-08-23 16:06:11 -0400

    server

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
+const 
 
-
-
-
-
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
+const port = process.env.port || 4000;
 
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
 
-
-
-
-
-
-
-
+app.use(express.static("./publc"));
 
 
 
