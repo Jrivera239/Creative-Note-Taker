c8487255752fb5077da6c8314a4808d25a7e265c
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-08-25 19:40:54 -0400

    creating routes

---
+++ publc/assets/routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../publc/index.html'));
+});
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../publc/notes.html'));
+});
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../publc/index.html'));
+})
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-const apiRoute = require('./routes/apiRoutes/notes');
-const htmlRoute = require('./routes/htmlRoutes');
 const express = require('express');
-const {db} = require('./db/db.json');
+const apiRoute = require('./routes/apiRoutes.js');
+const htmlRoute = require('./routes/htmlRoutes.js');
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static('publc'));
-app.use(express.urlencoded({esteended:true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
 app.listen(4000, () => {
-    console.log(`Server 4000!`);
+    console.log(`API server now on port http://localhost:${PORT}`)
 })
-
-
-
-
 
 
 
