59ba84f8196454690da2e3f7ee8583f04548f97b
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-08-25 21:04:39 -0400

    port number

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const apiRoute = require('./routes/apiRoutes.js');
 const htmlRoute = require('./routes/htmlRoutes.js');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('publc'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
-app.listen(4000, () => {
+app.listen(3001, () => {
     console.log(`API server now on port http://localhost:${PORT}`)
 })
 
