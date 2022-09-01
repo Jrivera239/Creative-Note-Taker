d49451ea2ad01d7a8423c90b1d9b716a867c2cc8
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-01 18:43:57 -0400

    qwerty

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(express.static('publc/assets'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('publc/assets'));
+
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
 app.listen(PORT, ()=> {
