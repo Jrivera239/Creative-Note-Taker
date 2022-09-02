f1cf9a26583702a87a95b0fd93368054da9523e4
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-01 20:10:14 -0400

    adding ; to missing spots

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const path = require ("path")
+const path = require ("path");
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../publc/index.html'));
 });
 router.get("/notes",(req, res) => {
-    res.sendFile(path.join(__dirname, "../publc/assets/notes.html"))
+    res.sendFile(path.join(__dirname, "../publc/assets/notes.html"));
 });
 router.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname, "../publc/assets/index.html"))
-})
-module.exports = router
+    res.sendFile(path.join(__dirname, "../publc/assets/index.html"));
+});
+module.exports = router;
