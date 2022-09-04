c96dad6913ab33ff504611b53016096fb88efe98
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-04 11:11:10 -0400

    trying to run heroku
    removing files and moving files

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -5,10 +5,7 @@ const { createNewNote, deleteNote } = require('../../lib/notes');
 const notes = require('../../db/notes.json');
 
 router.get('/notes', (req, res) => {
-    let result = (notes);
-    res.send(result);
-    console.log(result);
-    console.log(notes);
+    let result = (notes); res.send(result); console.log(result); console.log(notes);
 });
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
@@ -19,5 +16,4 @@ router.delete('/notes/:id', (req, res) => {
     const deletedNote = deleteNote(req.params.id, notes);
     res.send(notes)
 });
-
 module.exports = router;
