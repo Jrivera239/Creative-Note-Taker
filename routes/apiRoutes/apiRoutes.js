85bb361be47cd6a59d0788280421f624b824c71f
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-05 08:57:23 -0400

    wking on moving files and changing to old way

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,6 +1,34 @@
 const router = require('express').Router();
-const notesRoutes = require('../apiRoutes/notesRoutes');
+const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid');
+const note = require('../db/db.json')
 
-router.use(notesRoutes);
+router.get('/notes', (req, res) => {
+    res.json(note);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uniqid();
+
+    const temporalNote = req.body;
+
+    note.push(temporalNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(note));
+    res.json(temporalNote);
+});
+
+router.delete('/notes/:id', (req,res) =>{
+
+    const id = req.params.id;
+    
+    for (i = 0; i <note.length; i++) {
+        if(note[i].id === id){
+            note.splice(i,1);
+        }
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(note));
+    res.send(note);
+});
 
 module.exports = router;
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
-const { createNewNote, deleteNote } = require('../../lib/notes');
-const notes = require('../../db/notes.json');
 
-router.get('/notes', (req, res) => {
-    let result = (notes); res.send(result); console.log(result); console.log(notes);
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../publc/index.html'));
 });
-router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../publc/notes.html'));
 });
-router.delete('/notes/:id', (req, res) => {
-    const deletedNote = deleteNote(req.params.id, notes);
-    res.send(notes)
+
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../publc/index.html'));
 });
+
 module.exports = router;
